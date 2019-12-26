4b85871c1207abd604aa2997f6f98608b2d2633e
Mantas Stoškus <mantas.stoskus@netcode.lt>
2019-12-26 23:46:34 +0200

    throw exception if required num of arguments not passed

--- src/main/java/seb/task/App.java
+++ src/main/java/seb/task/App.java
@@ -3,29 +3,21 @@ package seb.task;
 public class App {
 
     public static void main(String[] args) {
-        try {
-            Long seedA = Long.valueOf(args[0]);
-            Long seedB = Long.valueOf(args[1]);
-            Generator<Long> generatorA = new NumberGeneratorImpl(seedA, 16807L, 2147483647L);
-            Generator<Long> generatorB = new NumberGeneratorImpl(seedB, 48271L, 2147483647L);
-            NumberMatcher matcher = new NumberMatcherImpl(8);
-            var matchCount = 0;
-            for (int x = 0; x < 1_000_000; x++) {
-                if (matcher.match(generatorA.generate(), generatorB.generate())) {
-                    matchCount++;
-                }
-            }
-            System.out.println("The number of times a comparison gave a positive result after 1'000'000 (1 million) comparisons.");
-            System.out.println(String.format("With parameters %d and %d the result is %d.", seedA, seedB, matchCount));
-        } catch (IndexOutOfBoundsException ex) {
-            printAndExit("Invalid number of arguments supplied.");
-        } catch (NumberFormatException ex) {
-            printAndExit("Could not parse number.");
+        if (args.length != 2) {
+            throw new RuntimeException("Two arguments required");
         }
-    }
-
-    private static void printAndExit(String msg) {
-        System.err.println(msg);
-        System.exit(1);
+        Long seedA = Long.valueOf(args[0]);
+        Long seedB = Long.valueOf(args[1]);
+        Generator<Long> generatorA = new NumberGeneratorImpl(seedA, 16807L, 2147483647L);
+        Generator<Long> generatorB = new NumberGeneratorImpl(seedB, 48271L, 2147483647L);
+        NumberMatcher matcher = new NumberMatcherImpl(8);
+        var matchCount = 0;
+        for (int x = 0; x < 1_000_000; x++) {
+            if (matcher.match(generatorA.generate(), generatorB.generate())) {
+                matchCount++;
+            }
+        }
+        System.out.println("The number of times a comparison gave a positive result after 1'000'000 (1 million) comparisons.");
+        System.out.println(String.format("With parameters %d and %d the result is %d.", seedA, seedB, matchCount));
     }
 }
