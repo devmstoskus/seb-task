80dd6dd919ad5561733a04596c4a511e7e45aaf9
Mantas Stoškus <mantas.stoskus@netcode.lt>
2019-12-26 21:24:08 +0200

    handle input

--- src/main/java/seb/task/App.java
+++ src/main/java/seb/task/App.java
@@ -1,6 +1,31 @@
 package seb.task;
 
 public class App {
+
     public static void main(String[] args) {
+        try {
+            Long seedA = Long.valueOf(args[0]);
+            Long seedB = Long.valueOf(args[1]);
+            NamedGenerator<Long> generatorA = new NumberGeneratorImpl(seedA, 16807L, "GeneratorA");
+            NamedGenerator<Long> generatorB = new NumberGeneratorImpl(seedB, 48271L, "GeneratorB");
+            NumberChecker checker = new BinaryNumberCheckerImpl(generatorA, generatorB);
+            var counter = 0;
+            for (int x = 0; x < 1_000_000; x++) {
+                if (checker.isMatch()) {
+                    counter++;
+                }
+            }
+            System.out.println("The number of times a comparison gave a positive result after 1'000'000 (1 million) comparisons.");
+            System.out.println(String.format("With parameters %d and %d the result is %d.", seedA, seedB, counter));
+        } catch (IndexOutOfBoundsException ex) {
+            printAndExit("Invalid number of arguments supplied.");
+        } catch (NumberFormatException ex) {
+            printAndExit("Could not parse number.");
+        }
+    }
+
+    private static void printAndExit(String msg) {
+        System.err.println(msg);
+        System.exit(1);
     }
 }
